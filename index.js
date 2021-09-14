c292c9788fc63fc8dfed51f5cc55ba670ebf023e
YuxiaoTang <867660224tyx@gmail.com>
2021-09-13 21:07:49 -0400

    add billing and client side app

--- index.js
+++ index.js
@@ -2,26 +2,37 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
-
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      keys: [keys.cookieKey]
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV == 'production'){
+  //Express will serve up production assets like main.js/css file
+  app.use(express.static('cllient/build'));
+  //Express will serve up the index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
