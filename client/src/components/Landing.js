f499ab4ebff442b6994176279afcaf04b9291681
YuxiaoTang <867660224tyx@gmail.com>
2021-09-17 12:13:25 -0400

    0917

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Landing = () => {
-  return(
-    <div style={{textAlign:'center'}}>
+  return (
+    <div style={{ textAlign: 'center' }}>
       <h1>
         Emaily!
       </h1>
-      Collect feedback from your users
+      Collect feedback form your users
     </div>
   );
 };
